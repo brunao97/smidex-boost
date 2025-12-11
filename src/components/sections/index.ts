fd08d20556599770a21cb36580dcb1b0f8af199a
Nicolas S <nicolasprodmac@gmail.com>
2025-12-11 01:01:18 -0300

    chore: organização imports

---
+++ src/components/sections/index.ts
@@ -0,0 +1,19 @@
+import HeroSection from "@/components/sections/hero-section";
+import LagProblemSection from "@/components/sections/lag-problem-section";
+import GamesSolutionSection from "@/components/sections/games-solution-section";
+import ProfessionalsSection from "@/components/sections/professionals-section";
+import OfferSection from "@/components/sections/offer-section";
+import AboutSection from "@/components/sections/about-section";
+import FaqSection from "@/components/sections/faq-section";
+import FooterSection from "@/components/sections/footer-section";
+
+export {
+  HeroSection,
+  LagProblemSection,
+  GamesSolutionSection,
+  ProfessionalsSection,
+  OfferSection,
+  AboutSection,
+  FaqSection,
+  FooterSection,
+};
